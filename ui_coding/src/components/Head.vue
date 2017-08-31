43d67d86611b588ad2042143997066e6762a6bcd
huchuanping <1137213572@qq.com>
2017-08-31 17:50:47 +0800

    blog

---
+++ ui_coding/src/components/Head.vue
@@ -0,0 +1,17 @@
+<template>
+    <el-menu :default-active="activeIndex2" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+    	<div class="container">
+	        <el-menu-item index="5"><a href=""><img src="../assets/logo.jpg" height="30" width="30"></a></el-menu-item>   
+	        <el-menu-item index="1">首页</el-menu-item>
+	        <el-menu-item index="2"><a href="">作品</a></el-menu-item>
+	        <el-menu-item index="3"><a href="">项目展示</a></el-menu-item>
+	        <el-menu-item index="4"><a href="">文章</a></el-menu-item>
+	        <el-menu-item index="5"><a href="">关于我</a></el-menu-item>   
+    	</div>
+    </el-menu>
+</template>
+<script>
+export default {}
+</script>
+<style>
+</style>
---
+++ ui_coding/src/views/Index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="home">
+    <nv-head></nv-head>
+  </div>
+</template>
+
+<script>
+import nvHead from '@/components/Head'
+export default {
+  components: {
+    'nv-head': nvHead
+  },
+  data () {
+    return {
+      msg: '我是首页'
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+}
+
+a {
+  color: #42b983;
+}
+</style>
